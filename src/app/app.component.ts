a60f7b238e712b63938083a5f7c24a99754e99cc
Geraldine Nunez L <gnunezl@psl.com.co>
2019-12-04 08:09:05 -0500

    structure with slider panels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { SplitComponent, SplitAreaDirective } from 'angular-split';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('split', { static: false }) split: SplitComponent;
+  @ViewChild('area1', { static: false }) area1: SplitAreaDirective;
+  @ViewChild('area2', { static: false }) area2: SplitAreaDirective;
+  direction = 'horizontal';
+  sizes = {
+    percent: {
+      area1: 20,
+      area2: 70,
+    },
+    pixel: {
+      area1: 120,
+      area2: '*',
+      area3: 160,
+    }
+  };
+
   title = 'flex-split';
+
+  dragEnd(unit, { sizes }) {
+    if (unit === 'percent') {
+      this.sizes.percent.area1 = sizes[0];
+      this.sizes.percent.area2 = sizes[1];
+    } else if (unit === 'pixel') {
+      this.sizes.pixel.area1 = sizes[0];
+      this.sizes.pixel.area2 = sizes[1];
+      this.sizes.pixel.area3 = sizes[2];
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularSplitModule } from 'angular-split';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularSplitModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
