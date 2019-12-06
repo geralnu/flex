7f5c0fa6c18005118bf308fdc125e87375fabb80
Geraldine Nunez L <gnunezl@psl.com.co>
2019-12-06 00:25:41 -0500

    zoom in-out panel, and side button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { SplitComponent, SplitAreaDirective } from 'angular-split';
+import panzoom from 'panzoom';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+    @ViewChild('panel', { static: false }) panel: ElementRef;
   @ViewChild('split', { static: false }) split: SplitComponent;
   @ViewChild('area1', { static: false }) area1: SplitAreaDirective;
   @ViewChild('area2', { static: false }) area2: SplitAreaDirective;
@@ -21,10 +24,23 @@ export class AppComponent {
       area3: 160,
     }
   };
-  isVisibleA: boolean = true;
-
+  panZoomController;
+  zoomLevels: number[];
+   currentZoomLevel: number;
+  // isVisibleA = true;
+    originalHeight = 397;
+    originalWidth = 276;
+    height = this.originalHeight;
+    width = this.originalWidth;
   title = 'flex-split';
 
+  ngAfterViewInit() {
+
+    this.zoomLevels = [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5, 3];
+    this.currentZoomLevel = this.zoomLevels[4];
+    // panzoom(document.querySelector('#scene'));
+    this.panZoomController = panzoom(this.panel.nativeElement);
+  }
   dragEnd(unit, { sizes }) {
     if (unit === 'percent') {
       this.sizes.percent.area1 = sizes[0];
@@ -36,12 +52,34 @@ export class AppComponent {
     }
   }
   log(x) {
-    console.log('dragEnd ', x.sizes, ' total > ', x.sizes.reduce((t, s) => t+s, 0))
-}
-collapseArea() {
-  console.log('oki');
-  this.sizes.pixel.area1 = 50;
-  //this.isVisibleA = !this.isVisibleA;
+    console.log('dragEnd ', x.sizes, ' total > ', x.sizes.reduce((t, s) => t + s, 0));
+  }
+  collapseArea() {
+    console.log('oki');
+    this.sizes.pixel.area1 = 50;
+    // this.isVisibleA = !this.isVisibleA;
+  }
+  zoom() {
+    const isSmooth = false;
+    const scale = this.currentZoomLevel;
+    if (scale) {
+      this.width = this.originalWidth * scale ;
+      this.height = this.originalHeight * scale ;
+      console.log(`width: ${this.width}, height: ${this.height}`);
+    }
+  }
 
-}
+  zoomToggle(zoomIn: boolean) {
+    const idx = this.zoomLevels.indexOf(this.currentZoomLevel);
+    if (zoomIn) {
+      if (typeof this.zoomLevels[idx + 1] !== 'undefined') {
+        this.currentZoomLevel = this.zoomLevels[idx + 1];
+      }
+    } else {
+      if (typeof this.zoomLevels[idx - 1] !== 'undefined') {
+        this.currentZoomLevel = this.zoomLevels[idx - 1];
+      }
+    }
+    this.zoom();
+  }
 }
